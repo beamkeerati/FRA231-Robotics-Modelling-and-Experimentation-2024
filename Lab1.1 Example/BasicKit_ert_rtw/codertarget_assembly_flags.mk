0f9148e7817b2922009b097646ec9334f7cce40d
beamkeerati <beamkeerati@hotmail.com>
2024-10-04 14:35:26 +0700

    Fix path

--- Lab1.1 Example/BasicKit_ert_rtw/codertarget_assembly_flags.mk
+++
@@ -1,5 +0,0 @@
-ASFLAGS_ADDITIONAL = -mcpu=cortex-m4 -mthumb -mlittle-endian -mthumb-interwork -mfpu=fpv4-sp-d16 -mfloat-abi=hard
-TARGET_LOAD_CMD = 
-TARGET_LOAD_CMD_ARGS = 
-TARGET_PKG_INSTALLDIR = C:/PROGRA~3/MATLAB/SUPPOR~1/R2024b/toolbox/target/supportpackages/stm32/stm32g4xx_based
-STACK_SIZE = 512
